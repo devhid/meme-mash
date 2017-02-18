0b2b5543d5258ae68fef1ca9e0ceca08ff5c18e1
Hid <mikeygulati@gmail.com>
2017-02-18 03:44:36 -0500

    Added inital http request handling.

--- meme-mash.go
+++ meme-mash.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome to MemeMash.")
+}
 
 func main() {
 	fmt.Println("Initializing...")
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
