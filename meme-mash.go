79f9bada545741bc287fbcdd0fde85a37819b566
spiderpig86 <slim679975@gmail.com>
2017-02-18 04:57:58 -0500

    Added Reddit bypass

--- meme-mash.go
+++ meme-mash.go
@@ -2,11 +2,32 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Welcome to MemeMash.")
+	url := "https://www.reddit.com/r/doge/"
+	//resp, _ := http.Get(url)
+	//bytes, _ := ioutil.ReadAll(resp.Body)
+
+	//fmt.Fprint(w, string(bytes))
+
+	//resp.Body.Close()
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36")
+	resp, err := client.Do(req)
+
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	//fmt.Fprint(w, string(bytes))
+	fmt.Println(string(bytes))
+	resp.Body.Close()
 }
 
 func main() {
@@ -14,4 +35,5 @@ func main() {
 
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
+
 }
